1c7db1faa0c912cacf3ef187a6ac4700d6964488
Uttam Kumar <uk89197@gmail.com>
2025-02-04 05:19:35 +0530

    BFS traversal of Graph

---
+++ tut2.cpp
@@ -0,0 +1,43 @@
+// Connnected Graph
+
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> bfs(int n, vector<int> adj[]){
+    vector<int> ans;
+    vector<int> vis(n+1,0);
+    for(int i=1;i<=n;i++){
+        if(!vis[i]){
+            queue<int> q;
+            q.push(i);
+            vis[i]=1;
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+                ans.push_back(node);
+                for(auto it:adj[node]){
+                    if(!vis[it]){
+                        q.push(it);
+                        vis[it]=1;
+                    }
+                }
+            }
+        }
+    }
+    return ans;
+}
+int main(){
+    int n,e;
+    cin>>n>>e;
+    vector<int> adj[n+1];
+    for(int i=0;i<e;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    vector<int>ans = bfs(n,adj);
+    for(auto it:ans){
+        cout<<it<<" ";
+    }
+    return 0;
+}
