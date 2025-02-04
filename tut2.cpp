b685be527e114248442a8df3f51b8ee55e0be749
Uttam Kumar <uk89197@gmail.com>
2025-02-04 11:09:20 +0530

    BFS  and DFS traversal of Graph

--- tut2.cpp
+++ tut2.cpp
@@ -1,26 +1,59 @@
-// Connnected Graph
+// // Connnected Graph
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// vector<int> bfs(int n, vector<int> adj[]){
+//     vector<int> ans;
+//     vector<int> vis(n+1,0);
+//     for(int i=1;i<=n;i++){
+//         if(!vis[i]){
+//             queue<int> q;
+//             q.push(i);
+//             vis[i]=1;
+//             while(!q.empty()){
+//                 int node = q.front();
+//                 q.pop();
+//                 ans.push_back(node);
+//                 for(auto it:adj[node]){
+//                     if(!vis[it]){
+//                         q.push(it);
+//                         vis[it]=1;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return ans;
+// }
+// int main(){
+//     int n,e;
+//     cin>>n>>e;
+//     vector<int> adj[n+1];
+//     for(int i=0;i<e;i++){
+//         int u,v;
+//         cin>>u>>v;
+//         adj[u].push_back(v);
+//         adj[v].push_back(u);
+//     }
+//     vector<int>ans = bfs(n,adj);
+//     for(auto it:ans){
+//         cout<<it<<" ";
+//     }
+//     return 0;
+// }
+
+
+
+// DFS 
 #include<bits/stdc++.h>
-using namespace std;
-vector<int> bfs(int n, vector<int> adj[]){
-    vector<int> ans;
-    vector<int> vis(n+1,0);
-    for(int i=1;i<=n;i++){
-        if(!vis[i]){
-            queue<int> q;
-            q.push(i);
-            vis[i]=1;
-            while(!q.empty()){
-                int node = q.front();
-                q.pop();
-                ans.push_back(node);
-                for(auto it:adj[node]){
-                    if(!vis[it]){
-                        q.push(it);
-                        vis[it]=1;
-                    }
-                }
-            }
+using namespace std;    
+vector<int> ans;
+vector<int>dfs(int n , vector<int> adj[], int s, vector<int>& vis){
+    vis[s]=1;
+    ans.push_back(s);
+    for(auto it: adj[s]){
+        if(!vis[it]){
+            dfs(n, adj, it, vis);
         }
     }
     return ans;
@@ -29,13 +62,15 @@ int main(){
     int n,e;
     cin>>n>>e;
     vector<int> adj[n+1];
+    vector<int> vis(n+1, 0);
     for(int i=0;i<e;i++){
         int u,v;
         cin>>u>>v;
-        adj[u].push_back(v);
         adj[v].push_back(u);
+        adj[u].push_back(v);
     }
-    vector<int>ans = bfs(n,adj);
+    int start = 1;
+    vector<int>ans = dfs(n,adj,start, vis);
     for(auto it:ans){
         cout<<it<<" ";
     }
