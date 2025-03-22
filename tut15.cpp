886ebd613f5d36d96d5842504d1c8c1337aabb09
Uttam Kumar <uk89197@gmail.com>
2025-03-22 13:33:55 +0530

    Topological Sort using DFS

--- tut15.cpp
+++ tut15.cpp
@@ -5,8 +5,36 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
+void dfs(int node, vector<vector<int>>& adj, vector<int>& visited, stack<int>& stack) {
+    visited[node] = true;
+    for (auto it : adj[node]) {
+        if (!visited[it]) {
+            dfs(it, adj, visited, stack);
+        }
+    }
+    stack.push(node);
+}
 int main()
 {
-
-return 0;
+    int n;
+    cin >> n;
+    vector<vector<int>> adj(n);
+    for (int i = 0; i < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    vector<int> vis(n, 0);
+    stack<int> st;
+    for (int i = 0; i < n; i++) {
+        if (!vis[i]) {
+            dfs(i, adj, vis, st);
+        }
+    }
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    return 0;
 }
