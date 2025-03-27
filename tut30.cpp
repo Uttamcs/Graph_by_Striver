ae4d10b1026192b433d57f19af9614e84e727f15
Uttam Kumar <uk89197@gmail.com>
2025-03-27 16:55:54 +0530

    Minimum Multiplications to reach End

---
+++ tut30.cpp
@@ -0,0 +1,41 @@
+// Minimum Multiplications to reach End
+// https://www.geeksforgeeks.org/problems/minimum-multiplications-to-reach-end/0
+
+#include <bits/stdc++.h>
+using namespace std;
+int minimumMultiplications(vector<int> &arr, int start, int end)
+{
+    // code here
+    queue<pair<int, int>> q;
+    if (start == end)
+        return 0;
+    vector<int> dist(100000, 1e9);
+    dist[start] = 0;
+    q.push({start, 0});
+    int mod = 100000;
+    while (!q.empty())
+    {
+        int node = q.front().first;
+        int steps = q.front().second;
+        q.pop();
+        for (auto it : arr)
+        {
+            int num = (it * node) % mod;
+            if (steps + 1 < dist[num])
+            {
+                dist[num] = steps + 1;
+                if (num == end)
+                    return steps + 1;
+                q.push({num, steps + 1});
+            }
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int start = 3, end = 30;
+    vector<int> arr = {2,5, 7};
+    cout << minimumMultiplications(arr, start, end) << endl;
+    return 0;
+}
